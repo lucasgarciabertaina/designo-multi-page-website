7a8511a45009d4eb42f233a1e50d579a557305a5
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2023-02-02 11:47:54 -0300

    Set up col.ors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.vue"],
   theme: {
-    extend: {},
+    fontFamily: {
+      jost: ["Jost", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        peach: "#E7816B",
+        black: "#1D1C1E",
+        white: "#FFFFFF",
+        "light-peach": "#FFAD9B",
+        "dark-gray": "#333136",
+        "light-gray": "#F1F3F5",
+      },
+    },
   },
   plugins: [],
 };
