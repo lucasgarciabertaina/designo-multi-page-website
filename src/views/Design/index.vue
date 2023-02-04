c79aaa3c9e8383e3aa5502a797f1474354dfbf96
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2023-02-04 12:37:00 -0300

    project organization

--- src/views/Design/index.vue
+++ src/views/Design/index.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from "../../components/TheWelcome.vue";
+import MainBanner from "../../components/sections/home/MainBaner.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <main-banner />
   </main>
 </template>
